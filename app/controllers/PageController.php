261b754e7589a2245e61e9fc558446142103bd47
Lukas Oppermann <lukas@vea.re>
2014-08-07 15:50:34 +0200

    fix

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -7,6 +7,8 @@ class PageController extends BaseController {
 	public function index($path)
 	{
 		$api = Api::get('.json?path='.$path.'&lang='.Config::get('app.locale'));
+		echo '.json?path='.$path.'&lang='.Config::get('app.locale');
+		return $api;
 		if( isset($api) && isset($api['data']))
 		{
 			return View::make($this->layout)->with('data', $api['data']);
