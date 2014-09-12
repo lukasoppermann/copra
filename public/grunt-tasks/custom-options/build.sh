9a26051ebf80ed85bc488810b90539c0130bd1f9
Lukas Oppermann <lukas@vea.re>
2014-09-12 11:32:31 +0200

    fix

--- public/grunt-tasks/custom-options/build.sh
+++ public/grunt-tasks/custom-options/build.sh
@@ -1,12 +1,21 @@
 # Build Script
 #
+gruntDir="public"
+#
 if [ -n "$(git status --porcelain)" ]; then
   echo -e "\n\033[33m[Warning]\033[0m Some files need to be checked in first.\n"
 else
   echo -e "\n\033[32mLet's create your build.\033[0m\n";
   git checkout build
   git merge master
-  # cd public
+  if [[ ${PWD##*/} == $gruntDir ]]; then
+  elif [[ -d $(find . -name $gruntDir -type d) ]]; then
+    cd $(find . -name $gruntDir -type d)
+  elif [[ -d '../public' ]]; then
+    cd '../public'
+  elif [[ -d '../../public' ]]; then
+    cd '../../public'
+  fi
   grunt make-build
   rm
   echo -e "\n\033[32mBuild branch status\033[0m\n";
@@ -16,5 +25,6 @@ else
     git add --all
     git commit -m "Build $(date +"%d.%m.%Y %H:%M:%S")"
     git push server build:master
+    git checkout  master
   fi
 fi
