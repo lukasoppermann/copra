24d7492d126c1e3b1710e0856755d4e508bc1aa3
Lukas Oppermann <lukas@vea.re>
2014-09-12 10:33:28 +0200

    adding command

---
+++ public/grunt-tasks/options/command.js
@@ -0,0 +1,11 @@
+// ----------------------------------------------
+// shell commands
+//
+module.exports = function(grunt, config) {
+  return {
+    // build: {
+    //   type : 'shell',
+    //   cmd  : 'build.sh'
+    // }
+  };
+};
