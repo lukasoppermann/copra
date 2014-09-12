60dc6e0568b0c7a4c68d9ec6dbff929598ca5629
Lukas Oppermann <lukas@vea.re>
2014-09-12 10:36:31 +0200

    working on build

--- public/grunt-tasks/custom-options/build.sh
+++ public/grunt-tasks/custom-options/build.sh
@@ -3,5 +3,9 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo -e "\n\033[33m[Warning]\033[0m Some files need to be checked in first.\n"
 else
-  echo -e "\n\033[32mLet's create your build.\033[0m";
+  echo -e "\n\033[32mLet's create your build.\033[0m\n";
+  git checkout build
+  git merge master
+  ck public
+  grunt make-build
 fi
