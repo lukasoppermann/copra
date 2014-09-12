103fb3421854b93663baef184545d64d39b90cef
Lukas Oppermann <lukas@vea.re>
2014-09-12 10:35:28 +0200

    update

--- public/grunt-tasks/custom-options/build.sh
+++ public/grunt-tasks/custom-options/build.sh
@@ -3,5 +3,5 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo -e "\n\033[33m[Warning]\033[0m Some files need to be checked in first.\n"
 else
-  echo "no changes";
+  echo -e "\n\033[32mLet's create your build.\033[0m";
 fi
