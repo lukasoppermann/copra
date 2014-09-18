abc8254c5865f5ddf6d45faa4482cdbfffe11fa0
Lukas Oppermann <lukas@vea.re>
2014-09-18 21:50:22 +0200

    make article_id unique

--- app/database/migrations/2014_08_24_111607_create_stream_table.php
+++ app/database/migrations/2014_08_24_111607_create_stream_table.php
@@ -19,7 +19,7 @@ class CreateStreamTable extends Migration {
 			// fields
 			$table->increments('id',true);
 			$table->integer('parent_id')->nullable()->default(NULL);
-			$table->integer('article_id');
+			$table->integer('article_id')->unique();
 			$table->string('stream',255);
 			$table->integer('position');
 		});
