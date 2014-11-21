fdeb7f279e0c29ed3dd8be229c84066e9908d496
Lukas Oppermann <lukas@vea.re>
2014-11-21 16:11:49 +0100

    improve build

--- public/grunt-tasks/build.sh
+++ public/grunt-tasks/build.sh
@@ -2,17 +2,15 @@
 #
 gruntDir="public"
 
-server="server"
-if [ ! -z "$1" ]; then
+if [ ! -z "$@" ]; then
   server=$1
+
+  if ! git remote | grep $server > /dev/null; then
+    echo -e "\n\033[31m[Error]\033[0m The remote $server does not exist\n"
+    return
+  fi
 fi
 
-if ! git remote | grep $server > /dev/null; then
-  echo -e "\n\033[31m[Error]\033[0m The remote $server does not exist\n"
-  return
-fi
-
-#
 if [ -n "$(git status --porcelain)" ]; then
 
   echo -e "\n\033[33m[Warning]\033[0m Some files need to be checked in first.\n"
@@ -44,11 +42,13 @@ else
   git add --all
   git commit -m "Build $(date +"%d.%m.%Y %H:%M:%S")"
 
-  read -p "Do you want to deploy this build to $server? (y/N) " yn
+  if [ ! -z "$server" ]; then
+    read -p "Do you want to deploy this build to ".$server."? (y/N) " yn
 
-  if [[ $yn =~ ^[Yy]$ ]]; then
-    echo -e "\n"
-    git push $server build:master
+    if [[ $yn =~ ^[Yy]$ ]]; then
+      echo -e "\n"
+      git push $server build:master
+    fi
   fi
   git checkout master
 
