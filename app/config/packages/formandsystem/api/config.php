147cfc9ac1acc5e3f497566099ba8d5b73f82a9a
Lukas Oppermann <lukas@vea.re>
2014-11-26 12:34:31 +0100

    fix

--- app/config/packages/formandsystem/api/config.php
+++ app/config/packages/formandsystem/api/config.php
@@ -11,7 +11,7 @@ return array(
   |
   */
   'config' => [
-    'url'       => 'http://newapi.formandsystem.local',
+    'url'       => $_ENV['FS_API_URL'],
     'version'   => '1',
     'client_id'     => $_ENV['FS_API_CLIENT_ID'],
     'client_secret' => $_ENV['FS_API_CLIENT_SECRET'],
