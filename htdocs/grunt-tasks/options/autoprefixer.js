83d2e7374620fecb792d5730886fe2552b42ac16
Lukas Oppermann <lukas@vea.re>
2014-12-01 23:51:34 +0100

    update

--- htdocs/grunt-tasks/options/autoprefixer.js
+++ htdocs/grunt-tasks/options/autoprefixer.js
@@ -4,7 +4,7 @@
 module.exports = function(grunt, config) {
   return {
 		options: {
-			browsers: ['last 4 version', 'ie 8', 'ie 9']
+			browsers: ['last 10 version', 'ie 8', 'ie 9']
 		},
 		css_files: {
 			src: config.cwd+config.package.dirs.css+'/*.css'
