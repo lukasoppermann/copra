e08d80e096b48bad4abf6327109497f02720164d
Lukas Oppermann <lukas@vea.re>
2014-12-08 23:40:58 +0100

    Revert "remove package config"
    This reverts commit 0ef09027f3168fa6d267e34ea00a6f9e57c43ae1.

---
+++ app/config/packages/formandsystem/api/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+  /*
+  |--------------------------------------------------------------------------
+  | Api Config
+  |--------------------------------------------------------------------------
+  |
+  | configuration for the api
+  |
+  */
+  'config' => [
+    'url'       => $_ENV['FS_API_URL'],
+    'version'   => '1',
+    'client_id'     => $_ENV['FS_API_CLIENT_ID'],
+    'client_secret' => $_ENV['FS_API_CLIENT_SECRET'],
+    'scope'         => 'content.read',
+    'cache'         => false,
+  ]
+
+);
