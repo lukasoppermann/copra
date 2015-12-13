6ee8f445d65231a48d16e4ee99ecd4ceb0059aac
Lukas Oppermann <lukas@vea.re>
2015-12-13 21:19:49 +0100

    add news

---
+++ app/database/seeds/FsNews.php
@@ -0,0 +1,236 @@
+<?php
+
+class FsNews extends Seeder {
+
+	/**
+	 * Auto generated seed file
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		\DB::table('fs_content')->insert(
+      array(
+    		array (
+    			'id' => 8,
+    			'article_id' => 8,
+    			'menu_label' => '',
+    			'link' => '',
+    			'published' => 1,
+    			'language' => 'de',
+    			'data' => '[
+    				{
+    					"content":[
+    						{
+    							"type": "block",
+    							"column": 2,
+    							"media": [
+    								{
+    									"src": "hirslanden-klinik.jpg",
+    									"description": "Erste Hirslanden Klinik startet mit COPRA"
+    								}
+    							],
+    							"content": "#Erste Hirslanden Klinik startet mit COPRA\\nDie Hirslanden Klinik Aarau startete im 3. Quartal 2014 mit COPRA auf der Intensivstation (IPS) und auf der An&#228;sthesie (OP) als Pilot für die gesamte Gruppe."
+    						}
+    						]
+    					}
+  				]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+    		),
+  			array (
+  				'id' => 9,
+  				'article_id' => 9,
+  				'menu_label' => '',
+  				'link' => '',
+  				'published' => 1,
+  				'language' => 'de',
+  				'data' => '[
+  					{
+  						"content":[
+  							{
+  								"type": "block",
+  								"column": 2,
+  								"content": "#Clinical Documentation Challenge (CDC)\\nCOPRA nahm erfolgreich an der diesjährigen CDC auf der DIVI14 in Hamburg teil. Das Thema war Medizincontrolling. Es nahmen 5 weitere PDMS-Hersteller an dem Wettbewerb teil."
+  							}
+  							]
+  						}
+  						]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+  			),
+  			array (
+  				'id' => 10,
+  				'article_id' => 10,
+  				'menu_label' => '',
+  				'link' => '',
+  				'published' => 1,
+  				'language' => 'de',
+  				'data' => '[
+  					{
+  						"content":[
+  							{
+  								"type": "block",
+  								"column": 2,
+  								"content": "#Neues Corporate Design\\nCOPRA bekommt ein neues Corporate Design und tritt zur DIVI14 erstmalig im neuen Aussehen auf."
+  							}
+  						]
+  					}
+  				]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+  			),
+  			array (
+  				'id' => 101,
+  				'article_id' => 101,
+  				'menu_label' => '',
+  				'link' => '',
+  				'published' => 1,
+  				'language' => 'de',
+  				'data' => '[
+  					{
+  						"content":[
+  							{
+  								"type": "block",
+  								"column": 2,
+  								"media": [
+  									{
+  										"src": "uniklinik-heidelberg.jpg",
+  										"description": "UniversitätsKlinikum Heidelberg auf COPRA6 umgestiegen"
+  									}
+  								],
+  								"content": "#UniversitätsKlinikum Heidelberg ist auf COPRA6 umgestiegen\\nZwischen dem 16. und 18. März 2015 wurden alle 70 Betten der Intensivstationen des Zentrums Chirurgie am UniversitätsKlinikum Heidelberg auf COPRA6 umgestellt und mit Fluidmanagement erfolgreich in Betrieb genommen."
+  							}
+  						]
+  					}
+  				]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+  			),
+  			array (
+  				'id' => 102,
+  				'article_id' => 102,
+  				'menu_label' => '',
+  				'link' => '',
+  				'published' => 1,
+  				'language' => 'de',
+  				'data' => '[
+  					{
+  						"content":[
+  							{
+  								"type": "block",
+  								"column": 2,
+  								"media": [
+  									{
+  										"src": "unimed-rostock.jpg",
+  										"description": "Universitätsmedizin Rostock startet mit COPRA6 auf der neurologischen ITS"
+  									}
+  								],
+  								"content": "#Universitätsmedizin Rostock startet mit COPRA6 auf der neurologischen ITS\\nDie Universitätsmedizin Rostock hat am 16. März 2015 das PDMS COPRA6 im Zentrum für Nervenheilkunde auf der Intensivstation, der Stroke Unit und der neurologischen Notaufnahme erfolgreich in Betrieb genommen.\\n\\nDamit wird COPRA6 an der Universitätsmedizin Rostock auf allen Intensivstationen, dem interdisziplinären Schockraum und im kompletten Zentral-OP inklusive Prämedikation und Aufwachraum eingesetzt."
+  							}
+  						]
+  					}
+  				]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+  			),
+  			array (
+  				'id' => 100,
+  				'article_id' => 100,
+  				'menu_label' => '',
+  				'link' => '',
+  				'published' => 1,
+  				'language' => 'de',
+  				'data' => '[
+  					{
+  						"content":[
+  							{
+  								"type": "block",
+  								"column": 2,
+  								"media": [
+  									{
+  										"src": "hirslanden-klinik.jpg",
+  										"description": "36. Internationalen Symposium für Anästhesie, Intensivtherapie, Notfallmedizin und Schmerztherapie"
+  									}
+  								],
+  								"content": "Besuchen Sie uns auf dem 36. Internationalen Symposium für Anästhesie, Intensivtherapie, Notfallmedizin und Schmerztherapie vom 24.01.-30.01.2015 in St. Anton am Arlberg, Österreich."
+  							}
+  						]
+  					}
+  				]',
+  				'created_at' => date("Y-m-d h:i:s"),
+  				'updated_at' => date("Y-m-d h:i:s"),
+  		  ),
+		  array (
+			  'id' => 103,
+			  'article_id' => 103,
+			  'menu_label' => '',
+			  'link' => '',
+			  'published' => 1,
+			  'language' => 'de',
+			  'data' => '[
+				  {
+					  "content":[
+						  {
+							  "type": "block",
+							  "column": 2,
+							  "media": [
+							  ],
+							  "content": "#COPRA6 ist Medizinprodukt der Klasse IIa\nSeit September 2015 ist COPRA6 zum Medizinprodukt der Klasse IIa zertifiziert"
+						  }
+					  ]
+				  }
+			  ]',
+			  'created_at' => date("Y-m-d h:i:s"),
+			  'updated_at' => date("Y-m-d h:i:s"),
+		),
+		array (
+			'id' => 104,
+			'article_id' => 104,
+			'menu_label' => '',
+			'link' => '',
+			'published' => 1,
+			'language' => 'de',
+			'data' => '[
+				{
+					"content":[
+						{
+							"type": "block",
+							"column": 2,
+							"media": [
+							],
+							"content": "#COPRA auf der CCB\nVom 24.-26. Februar 2016 finden Sie uns am Stand Z4 im Übergang zwischen Halle 4 und dem CCB auf dem Symposium Intensivmedizin und Intensivpflege in Bremen"
+						}
+					]
+				}
+			]',
+			'created_at' => date("Y-m-d h:i:s"),
+			'updated_at' => date("Y-m-d h:i:s"),
+	  ),
+	  array (
+		  'id' => 105,
+		  'article_id' => 105,
+		  'menu_label' => '',
+		  'link' => '',
+		  'published' => 1,
+		  'language' => 'de',
+		  'data' => '[
+			  {
+				  "content":[
+					  {
+						  "type": "block",
+						  "column": 2,
+						  "media": [
+						  ],
+						  "content": "#COPRA ist Hauptsponsor des Internationalen Symposium für Anästhesie, Intensivtherapie, Notfallmedizin und Schmerztherapie\nVom 23.-29. Januar 2016  ist die COPRA System GmbH Hauptsponsor des Internationalen Symposium für Anästhesie, Intensivtherapie, Notfallmedizin und Schmerztherapie in St. Anton"
+					  }
+				  ]
+			  }
+		  ]',
+		  'created_at' => date("Y-m-d h:i:s"),
+		  'updated_at' => date("Y-m-d h:i:s"),
+		),
+      )
+    );
+  }
+}
